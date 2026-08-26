7ef0c908c20ac8a3f8ff9d274abfb596b88a30df
tanisnus <taniss246@gmail.com>
2026-08-25 17:46:47 -0700

    assignment 8

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { getUserAnalytics, getUsersWithStats, searchTasks } = require("../controllers/analyticsController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
+
+router.use(jwtMiddleware);
 
 router.get("/users", getUsersWithStats);
 router.get("/users/:id", getUserAnalytics);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const taskController = require("../controllers/taskController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
+
+router.use(jwtMiddleware);
 
 router.get("/", taskController.index);
 router.post("/", taskController.create);
@@ -10,7 +13,4 @@ router.get("/:id", taskController.show);
 router.patch("/:id", taskController.update);
 router.delete("/:id", taskController.deleteTask);
 
-module.exports = router
-
-
-
+module.exports = router;
